9e862ab14460bf8ced0d504f3cc8a6f1dee91a42
110108 <31996737+110108@users.noreply.github.com>
2019-03-01 17:59:19 -0500

    finished v1

--- game.py
+++ game.py
@@ -4,10 +4,21 @@ import random
 #setup
 #screen
 sc = turtle.Screen()
+#hero
 hero = turtle.Turtle()
-hero.goto(random.randint(-200,-400),random.randint(-200,-400))
+hero.pu()
+hero.shape("turtle")
+hero.goto(-random.randint(200,400),-random.randint(200,400))
+#enemy
 enemy = turtle.Turtle()
+enemy.shape("triangle")
+enemy.pu()
+#coin
 coin = turtle.Turtle()
+coin.shape("circle")
+coin.pu()
+coin.goto(random.randint(-300,300),random.randint(-300,300))
+#random vars
 speed=0
 rot=0
 
@@ -15,11 +26,11 @@ sc.tracer(0)
 
 def w():
     global speed
-    speed = 0.15
+    speed = 1
 
 def s():
     global speed
-    speed = -0.15
+    speed = 1
 
 def stop():
     global speed
@@ -27,21 +38,44 @@ def stop():
 
 def a():
     global rot
-    rot = .5
+    rot = 1
 
 def d():
     global rot
-    rot = -.5
+    rot = -1
 
 def cease():
     global rot
     rot = 0
+
+def move(turtle, speed, rot):
+    turtle.fd(speed)
+    turtle.left(rot)
+    
 
 def screenWrap(turtle):
     if(turtle.xcor()>465):
         turtle.goto(-465,turtle.ycor())
     elif(turtle.xcor()<-465):
         turtle.goto(465,turtle.ycor())
+    if(turtle.ycor()>500):
+        turtle.goto(turtle.xcor(),-390)
+    elif(turtle.ycor()<-390):
+        turtle.goto(turtle.xcor(),390)
+
+def chase(target, this):
+    this.setheading(this.towards(target))
+    this.forward(0.15)
+
+def caught(target, this):
+    if(target.distance(this) < 20):
+        return True
+    return False
+def loss():
+    sc.bgcolor("red")
+
+def win():
+    sc.bgcolor("green")
 
 sc.onkeypress(w,"w")
 sc.onkeyrelease(stop, "w")
@@ -53,8 +87,12 @@ sc.onkeyrelease(cease, "d")
 sc.onkeypress(d,"d")
 sc.listen()
 
-while True:
+while ((not caught(hero,enemy))and(not caught(coin,hero))):
     sc.update()
-    hero.fd(speed)
-    hero.left(rot)
+    move(hero, speed/5, rot/2)
+    chase(hero, enemy)
+    if(caught(hero,enemy)):
+        loss()
+    if(caught(coin,hero)):
+        win()
     screenWrap(hero)
