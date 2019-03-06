7c85d376c6bbe55a6e4ca2ee205a0262415e4efa
110108 <31996737+110108@users.noreply.github.com>
2019-03-06 08:23:43 -0500

    Create enemy.py

---
+++ v2/enemy.py
@@ -0,0 +1,17 @@
+class enemy:
+    def __init__(self,xpos,ypos,speed):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.speed = speed
+
+    def moveTowards(target):
+        self.setheading(self.towards(target))
+        self.forward(speed)
+
+    def getX():
+        return xpos
+
+    def getY():
+        return ypos
+
+    
